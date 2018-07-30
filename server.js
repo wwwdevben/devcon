b9c6ca25acde1bac1a42e97b86cd1bb83525ac76
Benjamin Phillips <ben@Benjamins-MacBook-Pro.local>
2018-07-29 20:45:48 -0400

    added router and routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //DB config
@@ -16,6 +21,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
+
+//User routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
